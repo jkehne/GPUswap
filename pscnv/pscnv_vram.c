eb6e80fd87b5e479b0397d3db2685ee328d767a0
Marcin Kościelnicki <koriakin@0x04.net>
2010-05-30 18:49:15 +0000

    vram: print VRAM size and LSR period on load

--- pscnv/pscnv_vram.c
+++ pscnv/pscnv_vram.c
@@ -268,6 +268,9 @@ pscnv_vram_init(struct drm_device *dev)
 	 */
 	dev_priv->vram_rblock_size = rowsize * 3;
 
+	NV_INFO(dev, "VRAM: size 0x%llx, LSR period %llx\n",
+			dev_priv->vram_size, dev_priv->vram_rblock_size);
+
 	allmem = kmalloc (sizeof *allmem, GFP_KERNEL);
 	if (!allmem)
 		return -ENOMEM;
