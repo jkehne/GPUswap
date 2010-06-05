016b760248f821d865afa7db3628e793feda047a
Marcin Kościelnicki <koriakin@0x04.net>
2010-06-05 02:53:13 +0000

    Write down ioctl interface.

--- pscnv/pscnv_drm.h
+++ pscnv/pscnv_drm.h
@@ -66,9 +66,75 @@ struct drm_pscnv_gem_info {	/* n i */
 };
 #define PSCNV_GEM_CONTIG	0x00000001	/* needs to be contiguous in VRAM */
 
+/* for vspace_new and vspace_free */
+struct drm_pscnv_vspace_req {	/* n f */
+	uint32_t vid;		/* > < */
+};
+
+struct drm_pscnv_vspace_map {
+	uint32_t vid;		/* < */
+	uint32_t handle;	/* < */
+	uint64_t start;		/* < */
+	uint64_t end;		/* < */
+	uint32_t back;		/* < */
+	/* none defined yet */
+	uint32_t flags;		/* < */
+	uint64_t offset;	/* > */
+};
+
+struct drm_pscnv_vspace_unmap {
+	uint32_t vid;		/* < */
+	uint32_t _pad;
+	uint64_t offset;	/* < */
+};
+
+struct drm_pscnv_chan_new {
+	uint32_t vid;		/* < */
+	uint32_t cid;		/* > */
+	/* The map handle that can be used to access channel control regs */
+	uint64_t map_handle;	/* > */
+};
+
+struct drm_pscnv_chan_free {
+	uint32_t cid;		/* < */
+};
+
+struct drm_pscnv_obj_vdma_new {
+	uint32_t cid;		/* < */
+	uint32_t handle;	/* < */
+	uint32_t oclass;	/* < */
+	uint32_t flags;		/* < */
+	uint64_t start;		/* < */
+	uint64_t size;		/* < */
+};
+
+struct drm_pscnv_fifo_init {
+	uint32_t cid;		/* < */
+	uint32_t pb_handle;	/* < */
+	uint32_t flags;		/* < */
+	uint32_t slimask;	/* < */
+	uint64_t pb_start;	/* < */
+};
+
+struct drm_pscnv_obj_gr_new {
+	uint32_t cid;		/* < */
+	uint32_t handle;	/* < */
+	uint32_t oclass;	/* < */
+	uint32_t flags;		/* < */
+};
+
 #define DRM_PSCNV_GETPARAM           0x00	/* get some information from the card */
 #define DRM_PSCNV_GEM_NEW            0x20	/* create a new BO */
 #define DRM_PSCNV_GEM_INFO           0x21	/* get info about a BO */
 /* also uses generic GEM close, flink, open ioctls */
+#define DRM_PSCNV_VSPACE_NEW         0x22	/* Create a new virtual address space */
+#define DRM_PSCNV_VSPACE_FREE        0x23	/* Free a virtual address space */
+#define DRM_PSCNV_VSPACE_MAP         0x24	/* Maps a BO to a vspace */
+#define DRM_PSCNV_VSPACE_UNMAP       0x25	/* Unmaps a BO from a vspace */
+#define DRM_PSCNV_CHAN_NEW           0x26	/* Create a new channel */
+#define DRM_PSCNV_CHAN_FREE          0x27	/* Free a channel */
+#define DRM_PSCNV_OBJ_VDMA_NEW       0x28	/* Create a new vspace DMA object on a channel */
+#define DRM_PSCNV_FIFO_INIT          0x29	/* Initialises PFIFO processing on a channel */
+#define DRM_PSCNV_OBJ_GR_NEW         0x2a	/* Create a new GR object on a channel */
 
 #endif /* __PSCNV_DRM_H__ */
