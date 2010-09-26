bb0951cce604d1fc9019b67c046edaf45b36f7fd
Marcin Kościelnicki <koriakin@0x04.net>
2010-09-26 19:39:18 +0000

    nv50/vm: Fix reporting for NVAF.

--- pscnv/nv50_vm.c
+++ pscnv/nv50_vm.c
@@ -496,7 +496,7 @@ void nv50_vm_trap(struct drm_device *dev) {
 			nv_wr32(dev, 0x100c90, idx | i << 24);
 			trap[i] = nv_rd32(dev, 0x100c94);
 		}
-		if (dev_priv->chipset < 0xa3 || dev_priv->chipset >= 0xaa) {
+		if (dev_priv->chipset < 0xa3 || (dev_priv->chipset >= 0xaa && dev_priv->chipset <= 0xac)) {
 			s0 = trap[0] & 0xf;
 			s1 = (trap[0] >> 4) & 0xf;
 			s2 = (trap[0] >> 8) & 0xf;
