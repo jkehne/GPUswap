4ea666a1db018a024b914a26ced7c671142d21b2
Maarten Lankhorst <m.b.lankhorst@gmail.com>
2012-03-20 10:51:32 +0100

    kapitest: Fix drm_mode_fb_cmd2 test

--- pscnv/kapitest/drm_mode_fb_cmd2.c
+++ pscnv/kapitest/drm_mode_fb_cmd2.c
@@ -2,8 +2,5 @@
 #include "drm.h"
 #include "drm_crtc_helper.h"
 
-int dummy(struct drm_framebuffer *fb, struct drm_mode_fb_cmd2 *mode_cmd)
-{
-	drm_helper_mode_fill_fb_struct(fb, mode_cmd);
-	return 0;
-}
+struct drm_mode_fb_cmd2 mode_cmd;
+
