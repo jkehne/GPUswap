2e00cd4bdf77b9ccfa56b53b0a6a9c60540ce32b
Mike Harsch <mike@harschsystems.com>
2012-07-30 08:55:50 -0600

    fix logic for vga_switcheroo_register_client() signature tests

--- pscnv/kapitest/switcheroo_ops.c
+++ pscnv/kapitest/switcheroo_ops.c
@@ -4,6 +4,12 @@
 
 void dummy(struct drm_device *dev)
 {
+	/* 
+	 * kernel 3.5 added the vga_switcheroo_client_ops struct to replace
+	 * the 3 separate args to vga_switcheroo_register_client() bringing
+	 * the total arg count down to 2
+	 */
+
 	static const struct vga_switcheroo_client_ops foo_switcheroo_ops = {
 		.set_gpu_state = NULL,
 		.reprobe = NULL,
