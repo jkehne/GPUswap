a3759a211a041651f36b4b7f7aa6ca95a107d1cd
Stanislav Spassov <Stanislav.Spassov@student.kit.edu>
2014-09-19 17:33:04 +0200

    Much fancier build.sh and pscnv_load.sh scripts.
    build.sh needs more work in terms of e.g. a command line parameter to
    skip all the "make cleans" if one is sure that they are not needed.
    
    pscnv_load.sh unloads any currently loaded GPU driver,
    optionally performs reclocking via vbios patching and the blob,
    and loads pscnv. For now, a fixed nvafakebios parameter string is used
    when patching the bios. This may not work on other systems!
    Ideally, the script should be improved to automatically decide
    which bytes in the BIOS need to be replaced.
    
    pscnv/Makefile now uses uname -r to set its SYSSRC again. IMHO,
    any overrides to this should really be done from outside (e.g. the
    build.sh script could be made to patch the Makefile as necessary)

--- build.sh
+++ build.sh
@@ -1,25 +1,98 @@
-#! /bin/bash
-#KERNELVER=$(uname -r)
-KERNELVER="3.5.7.31"
+#!/bin/bash -eu
+# -e: Exit immediately if a command exits with a non-zero status.
+# -u: Treat unset variables as an error when substituting.
 
-. /etc/profile
-. ~/.bashrc
+# Builds pscnv and libpscnv. Currently doesn't install anything anywhere,
+# but calls switch-to-pscnv.sh to load up the produced module pscnv.ko
+# directly from the build folder
 
-cd pscnv
+# INPUT VARIABLES
+# ===============
+# Compiles from
+# $PSCNV_SOURCE_DIR  (default $HOME/pscnv )
+# in build directory:
+# $PSCNV_BUILD_DIR   (default $PSCNV_SOURCE_DIR/build )
+
+# SCRIPT BODY
+# ==========
+
+# Search "man bash" for "Assign Default Values" to understand these lines:
+: ${PSCNV_SOURCE_DIR:="$HOME/pscnv"}
+: ${PSCNV_BUILD_DIR:="$PSCNV_SOURCE_DIR/build"}
+echo -n "$(tput bold)"
+echo    "~~~ Dumping input variables and performing some checks..."
+echo -n "$(tput sgr0)"
+echo    "PSCNV_SOURCE_DIR=$PSCNV_SOURCE_DIR"
+echo    "PSCNV_BUILD_DIR=$PSCNV_BUILD_DIR"
+echo
+echo    "Will compile against your current kernel version: $(uname -r)"
+
+#Short aliases
+SRC_DIR="$PSCNV_SOURCE_DIR"
+BUILD_DIR="$PSCNV_BUILD_DIR"
+
+# Clean old stuff up
+echo
+echo "$(tput bold)~~~ Cleaning $SRC_DIR/pscnv (make clean; make distclean)...$(tput sgr0)"
+cd "$SRC_DIR/pscnv"
 make clean
-cd ../libpscnv
-make clean
-cd ..
-rm -rf build
-mkdir build
-cd build
-cmake ..
-make clean
-make
-mkdir -p /lib/modules/$KERNELVER/extra
-cp pscnv/pscnv.ko /lib/modules/$KERNELVER/extra
-depmod
-#cd ../test
+make distclean
+
+#echo
+#echo "$(tput bold)~~~ Cleaning $SRC_DIR/libpscnv (make clean)...$(tput sgr0)"
+#cd "$SRC_DIR/libpscnv"
 #make clean
+
+#echo
+#echo "$(tput bold)~~~ Cleaning $SRC_DIR/test (make clean)...$(tput sgr0)"
+#cd "$SRC_DIR/test"
+#make clean
+
+echo
+echo "$(tput bold)~~~ Removing build dir $BUILD_DIR...$(tput sgr0)"
+rm -rf "$BUILD_DIR"
+
+# Recreate build directory
+echo
+echo "$(tput bold)~~~ Creating and entering $BUILD_DIR...$(tput sgr0)"
+mkdir -pv "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+# cmake
+echo
+echo "$(tput bold)~~~ Invoking cmake $SRC_DIR...$(tput sgr0)"
+cmake "$SRC_DIR"
+
+# make driver and library
+echo
+echo "$(tput bold)~~~ Invoking make...$(tput sgr0)"
+make
+
+# make test
+#echo
+#echo "$(tput bold)~~~ Building tests...$(tput sgr0)"
+#cd "$SRC_DIR/test"
 #make
-#cd ..
+
+# Success
+PSCNV_KO_PATH="$BUILD_DIR/pscnv/pscnv.ko"
+echo
+echo -n "$(tput bold)$(tput setaf 2)"
+echo "~~~ Success! Pscnv was built!"
+echo -n "$(tput sgr0)"
+echo "To install and load into kernel: "
+echo "    sudo mkdir -p \"/lib/modules/\$(uname -r)/extra\""
+echo "    sudo cp \"$PSCNV_KO_PATH\" \"/lib/modules/\$(uname -r)/extra\""
+echo "    sudo depmod"
+echo "    sudo modprobe pscnv"
+echo
+echo "To load into kernel directly without installing:"
+echo "    sudo modprobe -v drm"
+echo "    sudo modprobe -v drm_kms_helper"
+echo "    sudo modprobe -v video"
+echo "    sudo modprobe -v i2c-algo-bit"
+echo "    sudo insmod \"$PSCNV_KO_PATH\""
+echo
+echo "To load using a \"smart\" script that removes current GPU driver and performs reclocking:"
+echo "    \"$SRC_DIR/pscnv_load.sh\""
+
--- pscnv_load.sh
+++ pscnv_load.sh
@@ -1,35 +1,165 @@
-#! /bin/bash
+#!/bin/bash -eu
+# -e: Exit immediately if a command exits with a non-zero status.
+# -u: Treat unset variables as an error when substituting.
 
-# see reclocking.txt 
-#
-# after reading it, you should have a command like the following:
+: ${PSCNV_NEEDS_KERNEL:="3.5.0-54-generic"}
+# Parse options ( http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash )
+# -f Try inserting pscnv even if a possibly incompatible kernel is detected
+# -x If pscnv happens to be already loaded, do nothing.
+# -r Reclock
+FORCE=0
+DONOTRELOAD=0
+RECLOCK=0
+while getopts "fxr" opt; do
+    case "$opt" in
+    f)   FORCE=1
+         ;;
+    x)   DONOTRELOAD=1
+         ;;
+    r)   RECLOCK=1
+    esac
+done
 
-/usr/local/bin/nvafakebios -e 613c:ff -e 6180:ff -e 61c4:ff vbios.rom
+shift $((OPTIND-1))
 
-# this will load vbios.rom, overwrite the bytes as given by -e parameters
-# and upload the modified bios image to the card
+# If pscnv is already loaded and we are not supposed to reload it,
+# exit silently
+if [[ $DONOTRELOAD == 1 ]] && (lsmod | grep -q pscnv); then
+  exit 0
+fi
 
-insmod nvidia.ko
+echo
+echo -n "$(tput bold)"
+echo    "This is: $0"
+echo    "~~~ Checking kernel compatibility with pscnv..."
+echo -n "$(tput sgr0)"
 
-# some calculation to force reclocking (almost anything should work)
-cd ~/gdev/test/cuda/bfs
-./bfs_nvidia
+# Check if running an old enough kernel
+if [[ ($FORCE == 0) && ("$(uname -r)" != "$PSCNV_NEEDS_KERNEL") ]]; then
+ echo "Your kernel might be incompatible with pscnv!"
+ echo
+ echo "According to \"uname -r\" you are running:"
+ echo -n "  "; uname -r
+ echo "which doesn't match the expected value by this script:"
+ echo "  $PSCNV_NEEDS_KERNEL"
+ echo
+ echo -n "$(tput bold)"
+ echo "To skip this check, use -f on the command line."
+ echo "Once you are sure that pscnv works with your kernel, "
+ echo "you can set PSCNV_NEEDS_KERNEL=\"$(uname -r)\""
+ echo "e.g. in your .bashrc, or just modify the default value"
+ echo "of that variable at the beginning of this script."
+ echo -n "$(tput sgr0)"
+ echo
+ echo -n "$(tput bold)$(tput setaf 1)"
+ echo "Aborting $0... This script did nothing!"
+ echo -n "$(tput sgr0)"
+ exit 1
+fi
 
-# nvidia-uvm gets automatically loaded
-rmmod nvidia-uvm
-rmmod nvidia
+echo
+echo -n "$(tput bold)"
+echo    "~~~ Checking for currently loaded GPU drivers..."
+echo -n "$(tput sgr0)"
+# Remove pscnv if it's currently loaded
+if lsmod | grep -q pscnv; then
+    echo "Pscnv is already loaded! Will attempt to remove now."
+    if test "$(cat /sys/class/vtconsole/vtcon1/bind)" = "1"; then
+      echo "Unbinding driver from console..."
+      sudo bash -c 'echo 0 > /sys/class/vtconsole/vtcon1/bind'
+    fi
+    echo "Removing module (rmmod pscnv)..."
+    sudo rmmod pscnv && echo "Pscnv successfully unloaded!"
+fi
 
-# the debug *_debug=? driver options just control pscnv's verbosity
-#
-# theoretically it shuld be also possible to load nouveau at this point
-#
-# Both stock pscnv and nouveau will fail to initialize, as the blob leaves
-# the card (video output code?) in some strange state that the open- source
-# drivers can't handle.
-# Simple solution: never mind! the pscnv in this branch is modified so that it
-# never even tries to initialize the screen, which triggers the fault.
-# All the GPGPU stuff is unaffected by this
-insmod ~/pscnv/pscnv/pscnv.ko vm_debug=0 pause_debug=2
+# Remove Nouveau if it's loaded
+if lsmod | grep -q nouveau; then
+    echo "Nouveau is loaded! Will attempt to remove now."
+    if test "$(cat /sys/class/vtconsole/vtcon1/bind)" = "1"; then
+      echo "Unbinding driver from console..."
+      sudo bash -c 'echo 0 > /sys/class/vtconsole/vtcon1/bind'
+    fi
+    echo "Removing module (modprobe -r nouveau)..."
+    sudo modprobe -rv nouveau && echo "Nouveau successfully unloaded!"
+    #/etc/init.d/consolefont restart
+fi
 
-# see dmesg output: pscnv print's available performance levels on init.
-# the line prefixed with 'c:' is the current level
+# Remove blob if it's loaded
+if lsmod | grep -q nvidia; then
+    echo "Nvidia blob is loaded! Will attempt to remove now."
+    if lsmod | grep -q nvidia_uvm; then
+        echo "Removing UVM module (rmmod nvidia_uvm)..."
+        sudo rmmod nvidia_uvm
+    fi
+    echo "Removing main driver module (rmmod nvidia)..."
+    sudo rmmod nvidia && echo "Nvidia blob successfully unloaded!"
+    #/etc/init.d/consolefont restart
+fi
+
+# Reclock GPU to highest frequency
+# See reclocking.txt
+if [[ $RECLOCK == 1 ]]; then
+    echo -n "$(tput bold)"
+    echo "~~~ Patching GPU bios to mark all but the highest power state as invalid..."
+    echo -n "$(tput sgr0)"
+    sudo nvagetbios > vbios.rom
+    nvbios vbios.rom > vbios.rom.dump
+    # TODO: Automatically find out the parameters for nvafakebios from vbios.rom.dump
+    NVAFAKEBIOS_PARAMS="-e 613c:ff -e 6180:ff -e 61c4:ff"
+    rm vbios.rom.dump
+    sudo nvafakebios $NVAFAKEBIOS_PARAMS vbios.rom
+    rm vbios.rom
+
+    echo -n "$(tput bold)"
+    echo "~~~ Loading blob driver..."
+    echo -n "$(tput sgr0)"
+    #sudo modprobe -vf nvidia
+    sudo insmod /lib/modules/$(uname -r)/updates/dkms/nvidia_340.ko
+    sudo insmod /lib/modules/$(uname -r)/updates/dkms/nvidia-340-uvm.ko
+
+    echo -n "$(tput bold)"
+    echo "~~~ Starting a GPU application to force reclocking..."
+    echo -n "$(tput sgr0)"
+    PREVDIR=`pwd`
+    cd ~jens/gdev/test/cuda/bfs
+    ./bfs_nvidia
+    cd $PREVDIR
+
+    echo -n "$(tput bold)"
+    echo "~~~ Unloading blob after reclocking..."
+    echo -n "$(tput sgr0)"
+    sudo rmmod nvidia-uvm
+    sudo rmmod nvidia
+fi
+
+# Load dependencies
+echo
+echo -n "$(tput bold)"
+echo    "~~~ Modprobing pscnv dependecies to ensure they are loaded..."
+echo -n "$(tput sgr0)"
+echo "Dependencies are: drm.ko drm_kms_helper.ko video.ko i2c-algo-bit.ko"
+sudo modprobe -v drm
+sudo modprobe -v drm_kms_helper
+sudo modprobe -v video
+sudo modprobe -v i2c-algo-bit
+
+: ${PSCNV_BUILD_DIR:="$HOME/pscnv/build"}
+PSCNV_KO_PATH="$PSCNV_BUILD_DIR/pscnv/pscnv.ko"
+echo
+echo -n "$(tput bold)"
+echo    "~~~ Loading pscnv module (sudo insmod \"$PSCNV_KO_PATH\")..."
+echo -n "$(tput sgr0)"
+sudo insmod "$PSCNV_KO_PATH"
+
+echo "Information from /sys/module/pscnv/sections:"
+echo -n ".text: ";   sudo cat /sys/module/pscnv/sections/.text
+echo -n ".data: ";   sudo cat /sys/module/pscnv/sections/.data
+echo -n ".rodata: "; sudo cat /sys/module/pscnv/sections/.rodata
+echo -n ".bss: ";    sudo cat /sys/module/pscnv/sections/.bss
+
+echo
+echo "$(tput bold)$(tput setaf 2)~~~ Success!$(tput sgr0)"
+if [[ $RECLOCK == 0 ]]; then
+echo "To reclock the GPU using the binary blob driver from nvidia and reload pscnv, run:"
+echo "    \"$0\" -r"
+fi
